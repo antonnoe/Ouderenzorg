b7f492befa9f30b9334a9a89f6d7dd70cedd683c
Anton Noë <63851898+antonnoe@users.noreply.github.com>
2026-01-07 17:21:08 +0100

    Add Tailwind CSS configuration file

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        maroon: '#800000',
+        softYellow: '#FFFDE7',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        mulish: ['Mulish', 'sans-serif'],
+      },
+      lineHeight: {
+        'relaxed-extra': '1.8',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
